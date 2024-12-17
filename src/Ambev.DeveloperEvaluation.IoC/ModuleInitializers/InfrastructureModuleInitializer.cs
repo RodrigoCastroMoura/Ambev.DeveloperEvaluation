1eec388b4d46bb25a17163f876b6976822382b7e
RodrigoCastroMoura <rodrigo_castro_moura@hotmail.com>
2024-12-16 21:14:10 -0300

    feat(feature/camada-servicos)foi implementado regras de serviços e os eventos de logs

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,10 +1,17 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Services;
+using Ambev.DeveloperEvaluation.Domain.Events;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Services;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using Rebus.Transport.InMem;
+
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
 
@@ -14,5 +21,17 @@ public class InfrastructureModuleInitializer : IModuleInitializer
     {
         builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
         builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+        builder.Services.AddScoped<ISaleItemRepository, SaleItemRepository>();
+        builder.Services.AddScoped<ISaleService, SaleService>();
+        builder.Services.AddRebus(configure => configure
+            .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "sales-queue"))
+            .Routing(r => r.TypeBased()
+                .Map<SaleModifiedEvent>("sales-queue")
+                .Map<SaleCreatedEvent>("sales-queue")
+                .Map<SaleCancelledEvent>("sales-queue")
+                .Map<ItemCancelledEvent>("sales-queue"))
+        );
     }
 }
